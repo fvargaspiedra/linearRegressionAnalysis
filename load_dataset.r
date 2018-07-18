b4a6d8becc7d3ebce35481014ee56f46fc6289aa
Javier Matas-Cabrera <javier.l.matias@gmail.com>
2018-07-18 15:51:09 -0400

    Added some more potential clean-ups.

--- load_dataset.r
+++ load_dataset.r
@@ -4,20 +4,22 @@ library(tibble)
 read_dataset = function(datafile) {
   data = read_csv(datafile)
   
+  ## hack to coerce all character columns from a tibble to factor
+  data = as.data.frame(unclass(as.data.frame(data)))
+
   ## These two neighborhoods only have 1 and 2 observations respectively.
   ## Remove them from the dataset
   data = data[-which(data$Neighborhood %in% c("GrnHill","Landmrk")),]
-  
-  
-  ## We don't need this column
+
+  ## These observations have 0 recorded bedrooms, might be invalid.
+  data = data[-which(data$Bedroom.AbvGr == 0),]
+
+  ## We don't need this column - this is an ID column.
   data = data[, -which(names(data) %in% c("PID"))]
-  
-  ## These numeric columns might make more sense as factor variables.
-  housing_data$Mo.Sold = as.factor(housing_data$Mo.Sold)
-  
-  ## hack to coerce all character columns from a tibble to factor
-  data = as.data.frame(unclass(as.data.frame(data)))
-  data = as_tibble(data)
+
+  ## This numeric columns might make more sense as factor variables.
+  data$Mo.Sold = as.factor(data$Mo.Sold)
+
+  return(as_tibble(data))
 }
 
-read_dataset("data/AmesHousing.csv")
